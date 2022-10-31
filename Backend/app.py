314393ed0c2293fcf67180e9a96966de744f29df
humberto <luislemusperez2014@gmail.com>
2022-10-30 22:56:01 -0600

    se agrego modificaciones al Frontend y se agrego los endpoint de crear recurso, crear categoria, crear configuracion

--- Backend/app.py
+++ Backend/app.py
@@ -1,26 +1,44 @@
-from flask import Flask, request
+from fileinput import filename
+from importlib.resources import path
+from tkinter import filedialog
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 
 from metodos import *
 
 lista_recursos=[]
 lista_categoria=[]
 lista_clientes=[]
+lista_consumo=[]
 metodo=Metodos
 app = Flask(__name__)
 
+#print(file)
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 @app.route("/Configuracion",methods=['POST'])
-def ObtenerDatos():
+def configuracion():
     perfil = request.files["confi"]
-    perfil1=perfil
-    metodo.leer_xmlconfiguracion(lista_recursos,perfil,lista_categoria,lista_clientes)
-   
-    print(perfil.filename)
-    #for x in lista_recursos:
-       # print(x.iden)
-   
+    filename= secure_filename(perfil.filename)
+    perfil.save(path('BaseDatos',filename))
+    file = filedialog.askopenfilename(title="abrir configuracion", filetypes=(("xml files","*.xml"),("all files", "*.*")))
+    ruta=file
+    
+    metodo.leer_xmlconfiguracion(lista_recursos,ruta,lista_categoria,lista_clientes)
+    consumo = request.files["consumo"]
+    filename1= secure_filename(consumo.filename)
+    consumo.save(path('BaseDatos',filename1))
+    file1 = filedialog.askopenfilename(title="abrir consumos", filetypes=(("xml files","*.xml"),("all files", "*.*")))
+    ruta=file1
+    metodo.leer_xmlconfiguracion_consumo(lista_consumo,ruta)
+    return render_template('index.html', respuesta1="el archivo de configuracion se envio correctamente", respuesta2="El mensaje de consumo se envio correctamente")
+
+@app.route("/Consultar_Datos")
+def Consultar_Datos():
+    for x in lista_recursos:
+        print(x.iden)
+    print("*************************")
     for x in lista_categoria:
         print(x.iden)
         for j in x.listaconfi:
@@ -31,7 +49,73 @@ def ObtenerDatos():
     for x in lista_clientes:
         print(str(i)+x.nombre)
         i+=1
-    return perfil.filename
-    
+        for j in x.listainstancia:
+            print(j.nombre)
+    print("********************")
+    for x in lista_consumo:
+        print(x.tiempo)
+    return "<p>Todo bien!</p>"  
+@app.route("/recurso")
+def recurso():
+    return render_template("crearrecurso.html")
+@app.route("/recurso/Crear_recurso",methods=['POST'])
+def crear_recurso():
+    iden=request.form['iden']
+    nombre=request.form['nombre']
+    abrebiatura=request.form['abre']
+    metrica=request.form['metrica']
+    tipo=request.form['tipo']
+    valorhora=float(request.form['hora'])
+    recurso= Recurso(iden,nombre,abrebiatura,metrica,tipo,valorhora)
+    lista_recursos.append(recurso)
+    return render_template('crearrecurso.html') 
+categoria=Categoria("","","","")
+@app.route("/categoria")
+def categoria():
+    return render_template("crearcategoria.html")
+@app.route("/categoria/Crear_categoria",methods=['POST'])
+
+def crear_categoria():
+    try:
+        global categoria
+        idencategoria=request.form['iden']
+        nombre=request.form['nombre']
+        descripcion=request.form['des']
+        carga=request.form['carga']
+        categoria1=Categoria(idencategoria,nombre,descripcion,carga)
+        categoria=categoria1
+        lista_categoria.append(categoria)
+        return "<p>categoria agregada con exito!</p>" 
+
+
+
+    except:
+        print("HUbo un error el ingreso de categoria")
+@app.route("/categoria/Crear_configuracion",methods=['POST'])
+
+def crear_configuracion():
+    try:
+        
+        idenconfi=request.form['iden']
+        nombre=request.form['nombre']
+        descripcion=request.form['des']
+        idrecurso=request.form['idrecu']
+        cantidadrecurso=request.form['cantidad']
+
+        confi=Configuracion(idenconfi,nombre,descripcion)
+        recurso=Recurso_confi(idrecurso,cantidadrecurso)
+        if metodo.existe_o_no_en_la_lista(lista_recursos,recurso)==True:
+
+            confi.lista_recurso_confi.append(recurso)
+        else:
+            print("el recurso ingresado no esta en el catalogo de recursos")
+        categoria.listaconfi.append(confi)
+        return "<p>categoria agregada con exito!</p>" 
+
+
+
+    except:
+        print("HUbo un error el ingreso de categoria")   
+
 if __name__ == '__main__':
     app.run(debug=True)
--- Backend/metodos.py
+++ Backend/metodos.py
@@ -16,102 +16,126 @@ class Metodos:
 
 
     def leer_xmlconfiguracion(listarecurso:list, archivo,listacategoria:list, listaclientes:list):
-        if archivo.readable():
-                print(True)
-                xml_data= ET.fromstring(archivo.read())
-                lst_empresas= xml_data.findall('listaRecursos')
-                for recursos in lst_empresas:
-                    recurso=recursos.findall("recurso")
-                    for recur in recurso:
-                        idenrecurso=""
-                        recu=Recurso("","","","","",0.0)
-                        iden=recur.attrib.values()
-                        for x in iden:
-                            idenrecurso=x
-                            recu.iden=idenrecurso
-                        nombrerecurso=recur.find('nombre').text
-                        recu.nombre=nombrerecurso
-                        abrevaturarecurso=recur.find('abreviatura').text
-                        recu.abrebiatura=abrevaturarecurso
-                        metrica=recur.find('metrica').text
-                        recu.metrica=metrica
-                        tiporecurso=recur.find('tipo').text
-                        recu.tipo=tiporecurso
-                        valorrecurso=recur.find('valorXhora').text
-                        recu.valor_hora=float(valorrecurso)
-                        listarecurso.append(recu)
-                #lectura de lista categoria
-                lst_categoria= xml_data.findall('listaCategorias')
-                for recursos in lst_categoria:
-                    categoria=recursos.findall("categoria")
-                    for cate in categoria:
-                        idencatego1=""
-                        catego=Categoria("","","","")
-                        idencatego=cate.attrib.values()
-                        for x in idencatego:
-                            idencatego1=x
-                            catego.iden=idencatego1
-                        nombrecatego=cate.find('nombre').text
-                        catego.nombre=nombrecatego
-                        categodescripcion=cate.find('descripcion').text
-                        catego.descripcion=categodescripcion
-                        cargacatego=cate.find('cargaTrabajo').text
-                        catego.cargaTrabajo=cargacatego
-                        listacategoria.append(catego)
-                        #lectura de listas de configuracion por categoria
-                        lst_conficatego=cate.findall("listaConfiguraciones")
-                        for conficatego in lst_conficatego:
-                            lst_conficatego_1=conficatego.findall('configuracion')
-                            for conf in lst_conficatego_1:
-                                codconfi=""
-                                conficate=Configuracion("","","")
-                                idconfcatego=conf.attrib.values()
-                                for x in idconfcatego:
-                                    codconfi=x
-                                    conficate.iden=codconfi
-                                nombreconficate=conf.find('nombre').text
-                                conficate.nombre=nombreconficate
-                                confidescrip=conf.find('descripcion').text
-                                conficate.descripcion=confidescrip
-                                catego.listaconfi.append(conficate)
-                                #lectura de lista de recursos de confi
-                                lst_recursoconfi=conf.findall("recursosConfiguracion")
-                                for confirecu in lst_recursoconfi:
-                                    lst_confi_recu=confirecu.findall("recurso")
-                                    
-                                    for recu_lst in lst_confi_recu:
-                                        cod_lst_recu=""
-                                        clase_recuso=Recurso_confi("",0.0)
-                                        id_lst_recu=recu_lst.attrib.values()
-                                        for  x in id_lst_recu:
-                                            cod_lst_recu=x
-                                            clase_recuso.iden=cod_lst_recu
-                                        numero_lst_recu=recu_lst.text
-                                        clase_recuso.numero=float(numero_lst_recu)
-                                        #print(clase_recuso.numero)
-                                        Metodos.incertar_lista_de_listas(catego.listaconfi,conficate,clase_recuso)
-                #lectura de lista de clientes
-                lst_cliente=xml_data.findall('listaClientes')
-                for lst_client in lst_cliente:
-                    lst_client_us=lst_client.findall("cliente")
-                    for cliente in lst_client_us:
-                        idcliente=""
-                        client=Cliente("","","","","","")
-                        cod_cliente= cliente.attrib.values()
-                        for x in cod_cliente:
-                            idcliente=X
-                            client.iden=idcliente
-                        nombrecliente=cliente.find('nombre').text
-                        client.nombre=nombrecliente
-                        usuariocliente=cliente.find('usuario').text
-                        client.usuario=usuariocliente
-                        clavecliente=cliente.find('clave').text
-                        client.clave=clavecliente
-                        direccioncliente=cliente.find('direccion').text
-                        client.direccion=direccioncliente
-                        correocliente=cliente.find('correoElectronico').text
-                        client.correo=correocliente
-                        listaclientes.append(client)
+        with open(archivo, encoding='utf-8') as file:
+            if file.readable():
+                    print(True)
+                    xml_data= ET.fromstring(file.read())
+                    lst_empresas= xml_data.findall('listaRecursos')
+                    for recursos in lst_empresas:
+                        recurso=recursos.findall("recurso")
+                        for recur in recurso:
+                            idenrecurso=""
+                            recu=Recurso("","","","","",0.0)
+                            iden=recur.attrib.values()
+                            for x in iden:
+                                idenrecurso=x
+                                recu.iden=idenrecurso
+                            nombrerecurso=recur.find('nombre').text
+                            recu.nombre=nombrerecurso
+                            abrevaturarecurso=recur.find('abreviatura').text
+                            recu.abrebiatura=abrevaturarecurso
+                            metrica=recur.find('metrica').text
+                            recu.metrica=metrica
+                            tiporecurso=recur.find('tipo').text
+                            recu.tipo=tiporecurso
+                            valorrecurso=recur.find('valorXhora').text
+                            recu.valor_hora=float(valorrecurso)
+                            listarecurso.append(recu)
+                    #lectura de lista categoria
+                    lst_categoria= xml_data.findall('listaCategorias')
+                    for recursos in lst_categoria:
+                        categoria=recursos.findall("categoria")
+                        for cate in categoria:
+                            idencatego1=""
+                            catego=Categoria("","","","")
+                            idencatego=cate.attrib.values()
+                            for x in idencatego:
+                                idencatego1=x
+                                catego.iden=idencatego1
+                            nombrecatego=cate.find('nombre').text
+                            catego.nombre=nombrecatego
+                            categodescripcion=cate.find('descripcion').text
+                            catego.descripcion=categodescripcion
+                            cargacatego=cate.find('cargaTrabajo').text
+                            catego.cargaTrabajo=cargacatego
+                            listacategoria.append(catego)
+                            #lectura de listas de configuracion por categoria
+                            lst_conficatego=cate.findall("listaConfiguraciones")
+                            for conficatego in lst_conficatego:
+                                lst_conficatego_1=conficatego.findall('configuracion')
+                                for conf in lst_conficatego_1:
+                                    codconfi=""
+                                    conficate=Configuracion("","","")
+                                    idconfcatego=conf.attrib.values()
+                                    for x in idconfcatego:
+                                        codconfi=x
+                                        conficate.iden=codconfi
+                                    nombreconficate=conf.find('nombre').text
+                                    conficate.nombre=nombreconficate
+                                    confidescrip=conf.find('descripcion').text
+                                    conficate.descripcion=confidescrip
+                                    catego.listaconfi.append(conficate)
+                                    #lectura de lista de recursos de confi
+                                    lst_recursoconfi=conf.findall("recursosConfiguracion")
+                                    for confirecu in lst_recursoconfi:
+                                        lst_confi_recu=confirecu.findall("recurso")
+                                        
+                                        for recu_lst in lst_confi_recu:
+                                            cod_lst_recu=""
+                                            clase_recuso=Recurso_confi("",0.0)
+                                            id_lst_recu=recu_lst.attrib.values()
+                                            for  x in id_lst_recu:
+                                                cod_lst_recu=x
+                                                clase_recuso.iden=cod_lst_recu
+                                            numero_lst_recu=recu_lst.text
+                                            clase_recuso.numero=float(numero_lst_recu)
+                                            #print(clase_recuso.numero)
+                                            Metodos.incertar_lista_de_listas(catego.listaconfi,conficate,clase_recuso)
+                    #lectura de lista de clientes
+                    lst_cliente=xml_data.findall('listaClientes')
+                    for lst_client in lst_cliente:
+                        lst_client_us=lst_client.findall("cliente")
+                        for cliente in lst_client_us:
+                            idcliente=""
+                            client=Cliente("","","","","","")
+                            cod_cliente= cliente.attrib.values()
+                            for x in cod_cliente:
+                                idcliente=X
+                                client.iden=idcliente
+                            nombrecliente=cliente.find('nombre').text
+                            client.nombre=nombrecliente
+                            usuariocliente=cliente.find('usuario').text
+                            client.usuario=usuariocliente
+                            clavecliente=cliente.find('clave').text
+                            client.clave=clavecliente
+                            direccioncliente=cliente.find('direccion').text
+                            client.direccion=direccioncliente
+                            correocliente=cliente.find('correoElectronico').text
+                            client.correo=correocliente
+                            listaclientes.append(client)
+                            lst_instancia=cliente.findall('listaInstancias')
+                            for lst_instan in lst_instancia:
+                                instancia=lst_instan.findall('instancia')
+                                for instan in instancia:
+                                    idintsnacia=""
+                                    claseintsncia=Instrancia("","","","","","")
+                                    codintancia= instan.attrib.values()
+                                    for x in codintancia:
+                                        idintsnacia=X
+                                        claseintsncia.iden=idintsnacia
+                                        idconfiintsancia=instan.find('idConfiguracion').text
+                                        claseintsncia.idconfi=idconfiintsancia
+                                        nombreintancia=instan.find('nombre').text
+                                        claseintsncia.nombre=nombreintancia
+                                        fechainicio=instan.find('fechaInicio').text
+                                        claseintsncia.fecha_inicio=fechainicio
+                                        estadointancia=instan.find('estado').text
+                                        claseintsncia.estado=estadointancia
+                                        fechafinal=instan.find('fechaFinal').text
+                                        claseintsncia.fecha_final=fechafinal
+                                        client.listainstancia.append(claseintsncia)
+
+
 
 
 
@@ -122,32 +146,43 @@ class Metodos:
 
                        
 
-        else:
-                print(False)
-    def leer_xmlconfiguracion_categoria(listacategoria:list, archivo):
-        if archivo.readable():
-                print(True)
-                xml_dat= ET.fromstring(archivo.read())
-                lst_empresas= xml_dat.findall('listaCategorias')
-                for recursos in lst_empresas:
-                    categoria=recursos.findall("categoria")
-                    for cate in categoria:
-                        idencatego=""
-                        catego=Categoria("","","","")
-                        idencatego=cate.attrib.values()
-                        for x in idencatego:
-                            idencatego=x
-                            catego.iden=idencatego
-                        nombrecatego=cate.find('nombre').text
-                        catego.nombre=nombrecatego
-                        categodescripcion=cate.find('descripcion').text
-                        catego.descripcion=categodescripcion
-                        cargacatego=cate.find('cargaTrabajo').text
-                        catego.cargaTrabajo=cargacatego
-                        listacategoria.append(catego)
-                        print(catego.iden)
+            else:
+                    print(False)
+    def leer_xmlconfiguracion_consumo(listaconsumo:list, archivo):
+     with open(archivo, encoding='utf-8') as file:
+            if file.readable():
+                    print(True)
+                    xml_data= ET.fromstring(file.read())
+                    lst_empresas= xml_data.findall('consumo')
+                    for consumo in lst_empresas:
+                        clase_consumo = Consumo("","",0.0,"")
+                        codigo=consumo.attrib.values()
+                        listas=[]
+                        for x in codigo:
+                            #print("la llvae es:" ,x)
+                            
+                            listas.append(x)
+                        clase_consumo.idencliente=listas[0]
+                        #print(confi.iden)
+                        clase_consumo.ideninstancia=listas[1]
+                        tiempo=consumo.find('tiempo').text
+                        clase_consumo.tiempo=tiempo
+                        fecha=consumo.find('fechaHora').text
+                        clase_consumo.fechahora=fecha
+                        listaconsumo.append(clase_consumo)
 
                        
 
+            else:
+                    print(False)
+    def existe_o_no_en_la_lista(lista:list, clase):
+        encontrado=False
+        for x in lista:
+            if x.iden==clase.iden:
+                encontrado=True
+                break
+        if encontrado==True:
+            return True
         else:
-                print(False)
+            return False
+            
